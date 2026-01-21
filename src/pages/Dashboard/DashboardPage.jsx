efd1d487b6f77ae33f2398f06eed33635fcccdd9
lucaidau <dotoan270806@gmail.com>
2026-01-22 03:16:24 +0700

    Mock Data for Dashboard

--- src/pages/Dashboard/DashboardPage.jsx
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -3,11 +3,23 @@ import Button from "../../components/common/Button";
 import SearchBar from "../../components/common/SearchBar";
 
 const DashboardPage = () => {
-  return (
-    <div>
-      <SearchBar></SearchBar>
-    </div>
-  );
+  const mockData = [
+    {
+      title: "To Do",
+      task: [
+        "Design homepage mockup",
+        "Design email pressure",
+        "Design edit design",
+      ],
+    },
+    {
+      title: "In Progress",
+      task: ["Write API documentation", "Write logic worksheet"],
+    },
+    { title: "Done", task: ["Team sync", "Write API documentation"] },
+  ];
+
+  return <div className=""></div>;
 };
 
 export default DashboardPage;
