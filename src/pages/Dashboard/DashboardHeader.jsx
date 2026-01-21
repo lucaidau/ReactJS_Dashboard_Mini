d1ca384acf5c111a230e2686dbc75e5fbfc57894
lucaidau <dotoan270806@gmail.com>
2026-01-22 03:36:33 +0700

    Done DashboardHeader

--- src/pages/Dashboard/DashboardHeader.jsx
+++ src/pages/Dashboard/DashboardHeader.jsx
@@ -4,9 +4,9 @@ import Button from "../../components/common/Button";
 
 const DashboardHeader = () => {
   return (
-    <div className="">
-      <h1 className=""></h1>
-      <div className="">
+    <div className="flex justify-between items-center mb-6">
+      <h1 className="text-4xl font-bold text-main-txt">My Tasks</h1>
+      <div className="flex gap-2 items-center">
         <SearchBar></SearchBar>
         <Button variant="primary"></Button>
       </div>
